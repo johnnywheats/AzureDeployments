c123c891a0398a76eb27b6208bd3e257d6ff7257
Johnny Wheats <johnnywheats@outlook.com>
2023-06-04 21:35:31 -0400

    updated output.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "network_interface_ids" {
-  value = azurerm_network_interface.internal.id
+output "network_interface_private_ip" {
+  description = "private ip addresses of the vm nics"
+  value       = "${azurerm_network_interface.vm.*.private_ip_address}"
 }
